dfd938bb6e7acf2c218c625c18f6bf06c8b9c1e4
Diogo Dourado <douradofadiogo@gmail.com>
2026-02-03 09:55:23 -0300

    [wip] Playground

--- automation_framework/config/environments/uat.py
+++
@@ -1,9 +0,0 @@
-from .base import EnvironmentConfig
-
-# UAT-specific configuration
-config = EnvironmentConfig(
-    base_url="https://www.selenium.dev",
-    headless=False,  # UAT often runs with browser visible
-    slow_mo=200,     # Slower execution for demo/debug
-    ignore_https_errors=True
-)
--- automation_framework/web/locators/common/selenium_home_locators.py
+++
@@ -1,7 +0,0 @@
-from dataclasses import dataclass
-from core.decorators import locator
-
-@locator(page_name="selenium_home", environment="uat")
-@dataclass
-class SeleniumHomeLocators:
-    about_selenium_link: str = "a[href='/about']"
